2ad613bf15705fc1e9d478560ebe31f77ad30160
“florMignini” <flormignini29@gmail.com>
2024-05-31 09:12:03 -0300

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["index.html", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundColor: "#2F5651",
+        text: "#BFD7B7",
+        "text-hover": "#abbfa4",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        // "bg-image-example": "url('/src/assets/imageExample.jpeg')",
+      },
+    },
   },
   plugins: [],
 }

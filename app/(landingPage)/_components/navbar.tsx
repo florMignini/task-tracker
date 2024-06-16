8db1c5481851dd93d3b5c8e8a4728bf7678dac3d
florMignini <flormignini29@gmail.com>
2024-06-16 19:51:04 -0300

    navbar update

--- app/(landingPage)/_components/navbar.tsx
+++ app/(landingPage)/_components/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <div className="fixed top-0 w-full h-14 px-4 shadow-lg ">
+    <div className="fixed top-0 w-full h-14 px-4 shadow-lg backdrop-blur-xl">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between text-black">
         <Logo />
         <div className=" h-auto space-x-4 md:block md:w-auto flex items-center justify-between w-full">
